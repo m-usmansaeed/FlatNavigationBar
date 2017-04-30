90fb379ef49cd4280c11ef5619abd130bdad25ad
M Usman Saeed <itsusman.saeed@gmail.com>
2017-04-30 17:15:18 +0500

    Initial Commit

---
+++ FlatNavigationBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlatNavigationBar
+//
+//  Created by M Usman Saeed on 30/04/2017.
+//  Copyright © 2017 M Usman Saeed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FlatNavigationBar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FlatNavigationBar
+//
+//  Created by M Usman Saeed on 30/04/2017.
+//  Copyright © 2017 M Usman Saeed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
